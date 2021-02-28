93ad8fdede0844bb193c9525d8208dde67afd734
SeinopSys <seinopsys@protonmail.com>
2021-02-28 03:23:06 +0100

    Bundle with rollup instead

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { Scrollchor } from './scrollchor';
-
 export * from './easing';
 export * from './helpers';
 export * from './scrollchor';
-
-// noinspection JSUnusedGlobalSymbols
-export default Scrollchor;
